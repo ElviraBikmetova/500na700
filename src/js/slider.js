e548bbf1afb0acc6184e879276b898ec2f3f2d24
Elvira <gaellru@gmail.com>
2023-09-21 17:09:08 +0300

    slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -4,10 +4,19 @@ function slider() {
     // console.log('object')
     var slider = tns({
         container: '.slider-block',
-        items: 1,
+        items: 2,
         slideBy: 'page',
-        autoplay: false,
-        controls: true
+        autoplay: false, // поменять на true
+        controls: true,
+        mouseDrag: true,
+        autoplayButtonOutput: false,
+        nav: false,
+        gutter: 20,
+        center: true,
+        startIndex: 1,
+        fixedWidth: 1240,
+        controlsPosition: "bottom",
+        controlsContainer: '.slider-controls'
       });
     // console.log('object')
 }
